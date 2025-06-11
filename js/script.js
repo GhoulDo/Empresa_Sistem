3f41e6115c07bbb2e8ff7f940afa25018669769a
numffy <franco76297124@gmail.com>
2025-06-10 22:50:15 -0500

    modularization:changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+//navbar component
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('[data-include]').forEach(async el => {
+    const file = el.getAttribute('data-include');
+    const resp = await fetch(file);
+    el.innerHTML = await resp.text();
+  });
+});
+
+
 // Navegación suave mejorada
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
